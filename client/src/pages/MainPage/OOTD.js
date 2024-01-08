21192fbc158327ee2f86f8ebb0ee36e67e5832d1
blee94 <135qudwls@naver.com>
2024-01-09 03:29:48 +0900

    almost done

--- client/src/pages/MainPage/OOTD.js
+++ client/src/pages/MainPage/OOTD.js
@@ -1,19 +1,40 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Modal from 'react-modal';
 import '../../css/OOTD.css';
+import axios from 'axios';
 
 function OOTD() {
   const [liked, setLiked] = useState({});
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [expandedImageUrl, setExpandedImageUrl] = useState('');
+  const [images, setImages] = useState([]);
 
-  const handleLikeClick = (area) => {
-    setLiked((prevLiked) => ({
-      ...prevLiked,
-      [area]: !prevLiked[area], // 해당 지역(area)의 liked 상태 토글
-    }));
+  const handleLikeClick = async (area) => {
+    try {
+      const image = images.find((img) => img.imageUrl === area);
+      if (image) {
+        const response = await axios.post(
+          `${process.env.REACT_APP_HOST}/imglike`,
+          {
+            userid: 'userid',
+            imageidx: image.postimgidx,
+          }
+        );
+
+        setLiked((prevLiked) => ({
+          ...prevLiked,
+          [area]: !prevLiked[area],
+        }));
+
+        console.log(response.data.message);
+      } else {
+        console.error('Image not found');
+      }
+    } catch (error) {
+      console.error('Like status change error:', error);
+    }
   };
+
   const handleImageClick = (imageUrl) => {
     setIsModalOpen(true);
     setExpandedImageUrl(imageUrl);
@@ -23,37 +44,42 @@ function OOTD() {
     setIsModalOpen(false);
     setExpandedImageUrl('');
   };
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_HOST}/getimg`
+        );
+        const imagesWithIndex = response.data.map((image, index) => ({
+          ...image,
+          postimgidx: index + 1,
+        }));
+        setImages(imagesWithIndex);
+        console.log('받아온 이미지 test: ', imagesWithIndex);
+      } catch (error) {
+        console.error('client 이미지 가져오기 에러:', error);
+      }
+    };
+
+    fetchImages();
+  }, []);
   return (
     <>
       <div className='OOTDContainer'>
-        <div className='area1'>
-          <img
-            src='img/OOTD1.jpg'
-            onClick={() => handleImageClick('img/OOTD1.jpg')}
-          ></img>
-          <div className='areaContent'>
-            [프렌드쉽]
-            <span
-              className={
-                liked['area1'] ? 'material-icons red' : 'material-icons'
-              }
-              onClick={() => handleLikeClick('area1')}
-            >
-              favorite
-            </span>
+        {images.map((image, index) => (
+          <div className={`area${index + 1}`} key={index}>
+            <img
+              src={image.imageUrl}
+              alt={`OOTD${index + 1}`}
+              onClick={() => handleImageClick(image.imageUrl)}
+            />
           </div>
-          <Modal
-            isOpen={isModalOpen && expandedImageUrl === 'img/OOTD1.jpg'}
-            onRequestClose={handleCloseModal}
-            contentLabel='Expanded Image Modal'
-          >
-            <button onClick={handleCloseModal}>Close</button>
-            <img src={expandedImageUrl} alt='Expanded' />
-          </Modal>
-        </div>
+        ))}
         <div className='area2'>
           <img
             src='img/OOTD2.jpg'
+            alt=''
             onClick={() => handleImageClick('img/OOTD2.jpg')}
           ></img>
           <div className='areaContent'>
@@ -79,6 +105,7 @@ function OOTD() {
         <div className='area3'>
           <img
             src='img/OOTD3.jpg'
+            alt=''
             onClick={() => handleImageClick('img/OOTD3.jpg')}
           ></img>
           <div className='areaContent'>
@@ -104,6 +131,7 @@ function OOTD() {
         <div className='area4'>
           <img
             src='img/OOTD4.jpg'
+            alt=''
             onClick={() => handleImageClick('img/OOTD4.jpg')}
           ></img>
           <div className='areaContent'>
@@ -129,6 +157,7 @@ function OOTD() {
         <div className='area5'>
           <img
             src='img/OOTD5.jpg'
+            alt=''
             onClick={() => handleImageClick('img/OOTD5.jpg')}
           ></img>
           <div className='areaContent'>
@@ -154,6 +183,7 @@ function OOTD() {
         <div className='area6'>
           <img
             src='img/OOTD6.jpg'
+            alt=''
             onClick={() => handleImageClick('img/OOTD6.jpg')}
           ></img>
           <div className='areaContent'>
@@ -179,6 +209,7 @@ function OOTD() {
         <div className='area7'>
           <img
             src='img/OOTD7.jpg'
+            alt=''
             onClick={() => handleImageClick('img/OOTD7.jpg')}
           ></img>
           <div className='areaContent'>
@@ -204,6 +235,7 @@ function OOTD() {
         <div className='area8'>
           <img
             src='img/OOTD8.jpg'
+            alt=''
             onClick={() => handleImageClick('img/OOTD8.jpg')}
           ></img>
           <div className='areaContent'>
